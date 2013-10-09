ad98f393a8239ada1c9f4c588c5ed59fab9c9bbe
Henning Glatter-Götz <henning@glatter-gotz.com>
2013-10-09 15:01:56 +0200

    Fix error message in BooleanValidator

--- src/HGG/ParameterValidator/Parameter/Validator/BooleanValidator.php
+++ src/HGG/ParameterValidator/Parameter/Validator/BooleanValidator.php
@@ -6,10 +6,11 @@ class BooleanValidator implements Validator
 {
     public function validate($value)
     {
+        $raw = $value;
         $value = filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
 
         if (null == $value) {
-            throw new \Exception(sprintf('The value \'%s\' is not of type boolean', $value));
+            throw new \Exception(sprintf('The value \'%s\' is not of type boolean', $raw));
         }
 
         return $value;
